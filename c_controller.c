6f7a736db33d26d4872dc2c44197c97404ec5f1c
Lê Nam <93436745+Le-Nguyen-Nhut-Nam@users.noreply.github.com>
2022-08-25 15:45:14 +0700

    Add files via upload
    Second file about PID

--- c_controller.c
+++ c_controller.c
@@ -5,7 +5,10 @@
 #include <webots/led.h>
 #include <webots/camera.h>
 #include <webots/supervisor.h>
-#define TIME_STEP 32
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TIME_STEP 8
 
 // Macro để định dạng byte sang nhị phân sử dụng trong hàm printf()
 #define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
@@ -23,6 +26,19 @@
 #define NB_GROUND_SENS 8
 #define NB_LEDS 5
 
+// Định nghĩa các tín hiệu của xe
+#define BLANK_SIGNAL  0
+#define MID   1
+#define LEFT  2
+#define RIGHT 3
+#define FULL_SIGNAL 4
+#define CURVE 5
+#define OUT_SPECIAL_SIGNAL 6
+
+#define MAX_SPEED 80
+
+
+
 // IR Ground Sensors
 WbDeviceTag gs[NB_GROUND_SENS];
 
@@ -32,239 +48,394 @@ WbDeviceTag led[NB_LEDS];
 // Motors
 WbDeviceTag left_motor, right_motor;
 
-/* Phần code được phép chỉnh sửa cho phù hợp */
-// Định nghĩa các tín hiệu của xe
-#define NOP  -1
-#define MID   0
-#define LEFT  1
-#define RIGHT 2
-#define FULL_SIGNAL 3
-#define BLANK_SIGNAL 4
-
-// Điểu chỉnh tốc độ phù hợp
-#define MAX_SPEED 20
-
 // Khai báo biến
-unsigned short threshold[NB_GROUND_SENS] = {350, 350, 350, 350, 350, 350, 350, 350};
+unsigned short gs_value[NB_GROUND_SENS] = {350, 350, 350, 350, 350, 350, 350, 350};
 unsigned char filted = 0b00000000;
 unsigned char preFilted = 0b00000000;
-int pos = MID;
-unsigned char intersectionDirect = LEFT;
-
-// Biến lưu giá trị tỉ lệ tốc độ của động cơ
+short pos = MID;
+float LeftSum,MidSum,RightSum=0;
 float left_ratio = 0.0;
 float right_ratio = 0.0;
 
-//Hàm đọc giá trị sensors
-unsigned char ReadSensors(void){
-  filted = 0b00000000;
-  unsigned short gs_value[NB_GROUND_SENS] = {0, 0, 0, 0, 0, 0, 0, 0};
+float PreviousLeftRatio=0.0;
+float PreviousRightRatio=0.0;
+
+//Rẽ ngã tư trái trả về 1, Rẽ ngã tư phải trả về 2, Tín hiệu bắt đầu vòng tròn trả về 3
+
+
+// Xác định kiểu thử thách: 1 là thử thách đơn giản, bao gồm chạy thẳng, chạy cong, chạy không có tín hiệu
+// 2,3 là Rẽ ngã tư trái, ngã tư phải 
+// 4 là Vòng tròn
+short Determination_Type=1;
+// Danh sách các flag cần thiết cho sử lý
+short FlagInCircle=0;
+short FlagOutCircle=0;
+short FlagIntersection=0;
+short FlagBlankSignal=0;
+
+// Hàm điều kiện
+
+// Hàm xác định tín hiệu
+short Determine_Position_Normal()
+{
+  if(LeftSum < 100 && MidSum < 100 && RightSum < 100)
+  {
+  Determination_Type=1;
+  return BLANK_SIGNAL; // trả về 0
+  }
   
-  for(int i=0; i<NB_GROUND_SENS; i++){
-    gs_value[i] = wb_distance_sensor_get_value(gs[i]);
-    // So sánh giá trị gs_value với threshold -> chuyển đổi sang nhị phân
-    if (gs_value[i] > threshold[i])
-      filted |= (0x01 << (NB_GROUND_SENS - i - 1));
+  else if (LeftSum<200 && MidSum > 350 && RightSum<200)
+  {
+  Determination_Type=1;
+  return MID; // TRẢ VỀ 1
   }
-  // Tắt comment để gỡ lỗi
-  /*for(int i=0; i<NB_GROUND_SENS; i++){printf("%d\t", gs_value[i]);}*/
-  return filted;
-}
-
-// Phần code điều khiển xe
-int DeterminePosition(unsigned char filted) {
-  switch (filted) {
-    case 0b00010000:
-    case 0b00001000:
-    case 0b00011000:
-    case 0b00111000:
-    case 0b00011100:
-        return MID;
-    case 0b00110000:
-    case 0b01100000:
-    case 0b11000000:
-    case 0b10000000:
-      return RIGHT;
-    case 0b00001100:
-    case 0b00000110:
-    case 0b00000011:
-    case 0b00000001:
-      return LEFT;
-    case 0b00000000:
-      return BLANK_SIGNAL;
-    case 0b11111111:
-    case 0b01111111:
-    case 0b11111110:
-    case 0b01111110:
-      return FULL_SIGNAL;
-    default: return NOP;
-  }
-}
-
-void GoStraight(unsigned char filted) {
-  switch (filted) {
-    case 0b00001000:
-      left_ratio = 1.0;
-      right_ratio = 0.9;
-      break;
-    case 0b00010000:
-      left_ratio = 0.9;
-      right_ratio = 1.0;
-      break;
-    case 0b00011000:
-      left_ratio = right_ratio = 1;
-    default:
-     left_ratio=right_ratio=1;
-      break;
-  }
-}
-
-void TurnLeft(unsigned char filted) {
-  switch (filted) {
-    case 0b00110000:
-      left_ratio = 0.7;
-      right_ratio = 0.9;
-      break;
-    case 0b01100000:
-      left_ratio = 0.55;
-      right_ratio = 0.8;
-      break;
-    case 0b11000000:
-      left_ratio = 0.3;
-      right_ratio = 0.7;
-      break;
-    case 0b10000000:
-      left_ratio = 0.2;
-      right_ratio = 0.7;
-      break;
-  }
-}
-
-void TurnRight(unsigned char filted) {
-  switch (filted) {
+  
+  else if(LeftSum>1000 && MidSum >300 && RightSum < 200)
+  {
  
-    case 0b00001100:
-      left_ratio = 0.9;
-      right_ratio = 0.7;
-      break;
-    case 0b00000110:
-      left_ratio = 0.8;
-      right_ratio = 0.55;
-      break;
-    case 0b00000011:
-      left_ratio = 0.7;
-      right_ratio = 0.3;
-      break;
-    case 0b00000001:
-      left_ratio = 0.7;
-      right_ratio = 0.2;
-      break;
+  Determination_Type=2;
+  return LEFT; // TRẢ VỀ 2  
   }
+  
+  else if(LeftSum<200 && MidSum>300 && RightSum>1000)
+  {
+  Determination_Type=3;
+  return RIGHT; //TRẢ VỀ 3 
   }
-void Blank(unsigned char filted){
-  switch(filted){
-  case 0b00000000:
+  
+  else if(LeftSum>400 && MidSum>800 && RightSum>400)
+  {
+ 
+  Determination_Type=4;
+  return FULL_SIGNAL; // TRẢ VỀ 4
+  }
+  
+  else
+  {
+  Determination_Type=1;
+  return CURVE;
+  }
+}
+short Determination_Position_Circle()
+{
+  if (LeftSum<200 && MidSum > 350 && RightSum<200)
+  return MID; // TRẢ VỀ 1
+  
+  else if(LeftSum>600 && MidSum<100 && RightSum>600)
+  return OUT_SPECIAL_SIGNAL; // Trả về 6
+  
+  else if(LeftSum>400 && MidSum>800 && RightSum>400)
+  return FULL_SIGNAL;
+  
+  else
+  return CURVE;
+}
+
+short Determine_Position_Left_Intersection()
+{
+  if(LeftSum < 100 && MidSum < 100 && RightSum < 100)
+  {
+  return BLANK_SIGNAL; // trả về 0
+  }
+  
+  else if (LeftSum<200 && MidSum > 350 && RightSum<200)
+  {
+  return MID; // TRẢ VỀ 1
+  }
+  
+  else if(LeftSum>1000 && MidSum >300 && RightSum < 200)
+  {
+  return LEFT; // TRẢ VỀ 2  
+  }
+  
+  else if(LeftSum<200 && MidSum>300 && RightSum>1000)
+  {
+  return RIGHT; //TRẢ VỀ 3 
+  }
+  
+  else if(LeftSum>400 && MidSum>800 && RightSum>400)
+  {
+  return FULL_SIGNAL; // TRẢ VỀ 4
+  }  
+  else
+  {
+  return CURVE;
+  }
+}
+short Determine_Position_Right_Intersection()
+{
+  if (LeftSum<200 && MidSum > 350 && RightSum<200)
+  return MID;
+  else if(LeftSum>400 && MidSum>800 && RightSum>400)
+  return FULL_SIGNAL;
+  else
+  return OUT_SPECIAL_SIGNAL;
+}
+// Hàm điều khiển
+
+void Normal_Control()
+{
+  switch(pos)
+  {
+  case MID: // Điều khiển chạy thẳng
+  left_ratio=1 - (LeftSum)/945.0;
+  right_ratio=1 - (RightSum)/945.0;
+  break;
+  
+  case BLANK_SIGNAL: // điều khiển chạy không tín hiệu
+  left_ratio=1.0;
+  right_ratio=1.0;
+  break;
+  
+  case CURVE: // điều khiển chạy đường cong
+  left_ratio=1 - (LeftSum)/945.0;
+  right_ratio=1 - (RightSum)/945.0;
+  break;
+  
+  case FULL_SIGNAL:
     left_ratio=1.0;
     right_ratio=1.0;
-  default:
-    left_ratio=1.0;
-    right_ratio=1.0;
+  break;
   }
 }
 
-
-void TurnLeftCorner(unsigned char filted) {
- switch(filted){
-   case 0b11111000:
-     left_ratio=0.0;
-     right_ratio=1.0;
-   case 0b11110000:
-     left_ratio=0.0;
-     right_ratio=1.0;
-   case 0b11100000:
-     left_ratio=0.0;
-     right_ratio=1.0;
-   case 0b11000000:
-     left_ratio=0.0;
-     right_ratio=1.0;
-   case 0b10000000:
-     left_ratio=0.0;
-     right_ratio=1.0;
-   default:
-     left_ratio=0;
-     right_ratio=1;  
-}
-}
-void TurnRightCorner(unsigned char filted) {
-  switch(filted){
-    case 0b00011111:
-      left_ratio=1.0;
-      right_ratio=0.0;
-    case 0b00001111:
-      left_ratio=1.0;
-      right_ratio=0.0;
-    case 0b00000111:
-      left_ratio=1.0;
-      right_ratio=0.0;
-    case 0b00000011:
-      left_ratio=1.0;
-      right_ratio=0.0;
-    default:
-      left_ratio=1.0;
-     right_ratio=0.0;
+void CircleControl()
+{
+  switch(pos)
+  {
+    case MID:
+    left_ratio=0.5;
+    right_ratio=0.5; 
+    
+    if(FlagOutCircle==1)
+          {
+            FlagOutCircle=0;
+            Determination_Type=1;
+          }
     break;
-}
-}
-void TurnLeftIntersection(unsigned char filted)
-{
-  switch(filted)
-  {
-    case 0b11111000:
-      left_ratio=0.0;
-      right_ratio=1.0;
-    case 0b11110000:
-      left_ratio=0.0;
-      right_ratio=1.0;
-    case 0b11100000:
-      left_ratio=0.0;
-      right_ratio=1.0;
-    case 0b11000000:
-      left_ratio=0.0;
-      right_ratio=1.0;
-    case 0b10000000:
-      left_ratio=0.0;
-      right_ratio=1.0;
-    default:
-      left_ratio=0.0;
-      right_ratio=1.0;
     
+    case FULL_SIGNAL:       
+          left_ratio=1.0;
+          right_ratio=0.0;  
+          
+          if(FlagInCircle==0)
+            FlagInCircle=1;                         
+    break;
+    
+    case CURVE:
+      left_ratio=1 - (LeftSum)/945.0;
+      right_ratio=1 - (RightSum)/945.0;
+    break;
+    
+    case OUT_SPECIAL_SIGNAL:   
+      left_ratio=1.0;
+      right_ratio=0.0;
+      
+      if(FlagInCircle==1)  
+      {
+        FlagOutCircle=1;
+        FlagInCircle=0;
+      }
+     break;
   }
 }
-void TurnRightIntersection(unsigned char filted)
+void LeftIntersectionControl()
 {
-  switch(filted)
+  switch(pos)
   {
-    case 0b00011111:
-      left_ratio=1.0;
-      right_ratio=0.0;
-    case 0b00001111:
-      left_ratio=1.0;
-      right_ratio=0.0;
-    case 0b00000111:
-      left_ratio=1.0;
-      right_ratio=0.0;
-    case 0b00000011:
-      left_ratio=1.0;
-      right_ratio=0.0;
-    case 0b00000001:
-      left_ratio=1.0;
-      right_ratio=0.0;
-    default:
-      left_ratio=1.0;
-      right_ratio=0.0;
-    
+  case MID:
+  left_ratio=(1 - (LeftSum)/945.0)*0.5;
+  right_ratio=(1 - (RightSum)/945.0)*0.5;
+  if(FlagIntersection==1)
+  Determination_Type=1;
+  if(FlagBlankSignal==1)
+  Determination_Type=1;
+  break;
+  
+  case LEFT:
+  left_ratio=right_ratio=0.5;
+  break;
+  
+  case RIGHT:
+  left_ratio=right_ratio=0.5;
+  break;
+  
+  case BLANK_SIGNAL:
+  left_ratio=PreviousLeftRatio;
+  right_ratio=PreviousRightRatio;
+  FlagBlankSignal=1;
+  break;
+  
+  case FULL_SIGNAL:
+  left_ratio=0.0;
+  right_ratio=0.9;
+  FlagIntersection=1;
+  break;
+  
+  case CURVE:
+  if(FlagIntersection==1)
+  {
+  left_ratio=0.0;
+  right_ratio=0.9;
+  }
+  else
+  {
+  left_ratio=1 - (LeftSum)/945.0;
+  right_ratio=1 - (RightSum)/945.0;
+  }
+  break;
+  
+  case OUT_SPECIAL_SIGNAL:
+  left_ratio=0.0;
+  right_ratio=0.9;
+  break;
   }
 }
+void RightIntersectionControl()
+{
+   switch(pos)
+  {
+  case MID:
+  left_ratio=(1 - (LeftSum)/945.0)*0.5;
+  right_ratio=(1 - (RightSum)/945.0)*0.5;
+  if(FlagIntersection==1)
+  Determination_Type=1;
+  if(FlagBlankSignal==1)
+  Determination_Type=1;
+  break;
+  
+  
+  case RIGHT:
+  left_ratio=right_ratio=0.5;
+  break;
+  
+  case BLANK_SIGNAL:
+  left_ratio=PreviousLeftRatio;
+  right_ratio=PreviousRightRatio;
+  FlagBlankSignal=1;
+  break;
+  
+  case FULL_SIGNAL:
+  left_ratio=1.0;
+  right_ratio=0.0;
+  FlagIntersection=1;
+  break;
+  
+  case CURVE:
+  if(FlagIntersection==1)
+  {
+  left_ratio=1.0;
+  right_ratio=0.0;
+  }
+  else
+  {
+  left_ratio=1 - (LeftSum)/945.0;
+  right_ratio=1 - (RightSum)/945.0;
+  }
+  break;
+  
+  case OUT_SPECIAL_SIGNAL:
+  left_ratio=1.0;
+  right_ratio=0.0;
+  break;
+  }
+}
+// Hàm điều khiển
+void Control()
+{
+  switch(Determination_Type)
+  {
+  case 1:
+  pos=Determine_Position_Normal();
+  Normal_Control();
+  break;
+  
+  case 2:
+  pos=Determine_Position_Left_Intersection();
+  LeftIntersectionControl();
+  break;
+  
+  case 3:
+  pos=Determine_Position_Right_Intersection();
+  RightIntersectionControl();
+  break;
+  
+  case 4:
+  pos=Determination_Position_Circle();
+  CircleControl();
+  break;
+  }
+}
+
+
+// In cac du lieu can thiet
+void PrintData()
+{
+    printf("\n");
+    printf("Left ratio: %f", left_ratio);
+    printf("                           ");
+    printf("Right ratio: %f", right_ratio);
+    printf("\n");
+    printf("Left Sum: %f",LeftSum);
+    printf("                           ");
+    printf("Mid Sum: %f",MidSum);
+    printf("                           ");
+    printf("Right Sum: %f",RightSum);
+    printf("\n");
+    printf("Position: %d", pos);
+    printf("            ");
+    printf("Mode: %d", Determination_Type);
+    printf("\n");
+};
+// Tính tổng từng bên.
+void CalculateSum(int i, short Value)
+{
+  switch(i)
+  {
+  case 0:
+  LeftSum = LeftSum + Value - 120;
+  break;
+  case 1:
+  LeftSum = LeftSum + Value - 120;
+  break;
+  case 2:
+  LeftSum = LeftSum + Value - 120;
+  break;
+  
+  case 3:
+  MidSum = MidSum + Value - 120;
+  break;
+  case 4:
+  MidSum = MidSum + Value - 120;
+  break;
+  
+  case 5:
+  RightSum = RightSum + Value - 120;
+  break;
+  case 6:
+  RightSum = RightSum + Value - 120;
+  break;
+  case 7:
+  RightSum = RightSum + Value - 120;
+  break;
+  }
+}
+
+//Hàm đọc giá trị sensors
+void ReadSensors(void)
+{
+  
+  for(int i=0; i<NB_GROUND_SENS; i++)
+  {
+    gs_value[i] = wb_distance_sensor_get_value(gs[i]);
+    printf("%d ",gs_value[i]);
+    
+    CalculateSum(i,gs_value[i]);
+  }
+
+}
+
+// Xác định các vị trị lệch và các tín hiệu bắt được của xe
 
 void constrain(float *value, float min, float max) {
   if (*value > max) *value = max;
@@ -311,69 +482,25 @@ int main() {
     wb_robot_step(TIME_STEP);
     
     // Đọc giá trị của sensors
-    filted = ReadSensors();
-    // Xác định tín hiệu của line đường
-    pos = DeterminePosition(filted);
-    // Xác định hướng rẽ của ngã tư
-    if (filted == 0b11110000 || filted == 0b11111000)
-      intersectionDirect = LEFT;
-    else if (filted == 0b00001111 || filted == 0b00011111)
-      intersectionDirect = RIGHT;
-    // In ra màn hình giá trị của filted ở dạng nhị phân
-    printf("Position: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(filted));
+    ReadSensors();
+    PrintData();
+    Control();  
     printf("\n");
+ 
     
-    // Điều khiển xe
-    if (pos == MID) {
-      GoStraight(filted);
-    }
-    else if (pos == RIGHT) {
-      TurnLeft(filted);
-    }
-    else if (pos == LEFT) {
-      TurnRight(filted);
-    }
-    else if (pos == BLANK_SIGNAL) {
-      if (preFilted == 0b11000000 || preFilted == 0b11100000 || preFilted == 0b11110000 || preFilted == 0b11111000)
-      {
-        TurnLeftCorner(filted);
-        
-        printf("Turn left corner\n");
-      }
-      else if (preFilted == 0b00000011 || preFilted == 0b00000111 || preFilted == 0b00001111 || preFilted == 0b00011111)
-      {
-        TurnRightCorner(filted);
-        printf("Turn right corner\n");
-      }
-      else if (preFilted == 0b00011000 || preFilted == 0b00010000 || preFilted == 0b00001000)
-      {
-        Blank(filted);
-        // Điều khiển xe mất line
-        printf("Lost of road markings\n");
-      }
-    }
-    else if (pos == FULL_SIGNAL)
-    {
-      if (intersectionDirect == RIGHT) {
-        TurnRightIntersection(filted);
-        // Điều khiển xe rẽ ngã tư trái
-        printf("RIGHT Intersection\n");
-      }
-      else if (intersectionDirect == LEFT) {
-        TurnLeftIntersection(filted);
-        // Điều khiển xe rẽ ngã tư trái
-        printf("LEFT Intersection\n");
-      }
-    }
-    
-    preFilted = filted;
-     
     // Giới hạn tỉ lệ tốc độ của động cơ
+    PreviousLeftRatio=left_ratio;
+    PreviousRightRatio=right_ratio;
+    
     constrain(&left_ratio, 0, 1);
     constrain(&right_ratio, 0, 1);   
     // Điều chỉnh tốc độ động cơ
     wb_motor_set_velocity(left_motor, left_ratio * MAX_SPEED);
     wb_motor_set_velocity(right_motor, right_ratio * MAX_SPEED);
+    
+    LeftSum=0;
+    RightSum=0;
+    MidSum=0;
   };
 
   wb_robot_cleanup();
